d95f87f81d6c981d43f43650f5e338ca449d284c
Lucio Gonzalez <lucgosa2@gmail.com>
2024-04-08 11:11:03 +0200

    first commit

--- destino.php
+++ destino.php
@@ -4,19 +4,10 @@
 <body>
 <?php
 
-
-	$la_fecha_1 = $_POST["fecha1"];
-	$la_fecha_2 = $_POST["fecha2"];
-
-	$la_fecha_timestamp_1 = strtotime($la_fecha_1);
-	$la_fecha_timestamp_2 = strtotime($la_fecha_2);
-
-	$diferencia_de_fechas = $la_fecha_timestamp_2 - $la_fecha_timestamp_1;
-
-	$resultado_en_dias = $diferencia_de_fechas/86400;
-
-	echo round($resultado_en_dias);
-
+	$fechayhora = $_POST["fecha"] . " " . $_POST["hora"];
+	$fechayhora_convertida = strtotime($fechayhora);
+	$diferencia = time() - $fechayhora_convertida;
+	echo $diferencia;
 	
 ?>
 </body>
